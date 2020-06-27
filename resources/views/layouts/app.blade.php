71fab1783d0517857c769ed6d5805ae456520a2b
Djaizansi <youcef.jallali@gmail.com>
2020-06-27 13:02:02 +0200

    Navbar chemin permission Admin/client

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,14 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css" integrity="sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q" crossorigin="anonymous">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'CarLocation') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -49,23 +50,48 @@
                                 </li>
                             @endif
                         @else
+
+                            @if (Auth::user()->roles === 'admin')
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ __('Voiture') }} <i class="fas fa-car"></i><span class="caret"></span>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('boutique') }}">{{ __('Afficher tous les véhicules') }}</a>
+                                        <a class="dropdown-item" href="{{ route('form') }}">{{ __('Créer un véhicule') }}</a>
+                                        <!-- <div class="dropdown-divider"></div> -->
+                                    </div>
+                                </li>
+                            @elseif (Auth::user()->roles === 'client')
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('boutique') }}">{{ __('Boutique') }} <i class="fas fa-store"></i></a>
+                                </li>
+                                
+                                <!-- <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('panier') }}">{{ __('Panier') }} <i class="fas fa-shopping-cart"></i></a>
+                                </li> -->
+                            @endif
+
                             <li class="nav-item dropdown">
+                                
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                    <a class="dropdown-item" href="{{ route('profile.edit', Auth::user()->id) }}"><i class="fa fa-btn fa-user"></i> {{ __('Profil') }}</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" style= "color: red;" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i> {{ __('Deconnexion') }}</a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
                             </li>
+                            
                         @endguest
                     </ul>
                 </div>
