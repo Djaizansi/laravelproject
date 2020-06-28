8a3e1b9904d4019005cd233b8667297fb43ed3d4
Djaizansi <youcef.jallali@gmail.com>
2020-06-28 23:54:03 +0200

    Mise en place du systeme de location pour louer un véhicule ou afficher s'il est disponible ou non

--- app/Http/Controllers/VoitureController.php
+++ app/Http/Controllers/VoitureController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Location;
 use App\Marque;
 use App\Modele;
 use App\Voiture;
 use Intervention\Image\Facades\Image;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class VoitureController extends Controller
 {
@@ -18,8 +20,9 @@ class VoitureController extends Controller
      */
     public function index()
     {
+        $locateByUser = Location::where('id_user', '=', Auth::user()->id)->get()->last();
         $voiture = Voiture::all();
-        return view('voitures.boutique',['voiture' => $voiture]);
+        return view('voitures.boutique',['voiture' => $voiture,'locateByUser'=>$locateByUser]);
     }
 
     /**
@@ -67,12 +70,13 @@ class VoitureController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    public function showForm()
+    public function showForm($id=null)
     {
         /* $date = Carbon::today()->format('d/m/Y'); */
+        $voiture = isset($id) ? Voiture::find($id) : '';
         $marque = Marque::all();
         $modele = Modele::with(['marques:id,nom'])->get();
-        return view('voitures.showForm',['marque'=>$marque,'modele'=>$modele]);
+        return view('voitures.showForm',['marque'=>$marque,'modele'=>$modele, 'voiture' => $voiture]);
     }
 
     public function ajax()
@@ -91,7 +95,8 @@ class VoitureController extends Controller
 
     public function show($id)
     {
-        //
+        $voiture = Voiture::find($id);
+        return view('voitures.show',['voiture'=>$voiture]);
     }
 
     /**
@@ -114,7 +119,37 @@ class VoitureController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate(
+            [
+                'date' => 'required|date|before_or_equal:tomorrow',
+                'marque' => 'required',
+                'modele' => 'required',
+                'prix' => 'required',
+                'description' => 'required|string',
+                'kilometrage' => 'required|int',
+                'image' => 'image|mimes:jpeg,jpg,png|max:4000|nullable'
+            ], // rules
+            ['date.before_or_equal' => 'La date d\'immatriculation ne peut pas être supérieur à la date du jour'] // Message$validatedData = 
+        );
+
+        $voiture = Voiture::find($id);
+        $voiture->id_marque = $request->marque;
+        $voiture->id_modele = $request->modele;
+        $voiture->prix = $request->prix;
+        $voiture->couleur = $request->couleur;
+        $voiture->date_immat = $request->date;
+        $voiture->description = $request->description;
+        $voiture->kilometrage = $request->kilometrage;
+        if(!empty($request->image)){
+            if($request->hasFile('image')){
+                $image = $request->file('image');
+                $filename = time(). '.' . $image->getClientOriginalExtension();
+                Image::make($image)->resize(300,300)->save( public_path('uploads/voitures/' . $filename) );
+            }
+            $voiture->image = $filename;
+        }
+        $voiture->save();
+        return redirect()->route('home')->with('status','Le véhicule a bien été modifié');
     }
 
     /**
@@ -125,6 +160,8 @@ class VoitureController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $voiture = Voiture::find($id);
+        $voiture->delete();
+        return redirect()->route('boutique')->with('status','Le véhicule a bien été supprimé');
     }
 }
