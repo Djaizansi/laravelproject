1b85a610b0423a5fc79c13b5f5049f5ce8f50c82
Djaizansi <youcef.jallali@gmail.com>
2020-06-29 00:04:09 +0200

    Modification du formulaire pour integrer de l'ajax et pouvoir supprimer les données existants afin de ne pas avoir de données en doublon

--- resources/views/voitures/showForm.blade.php
+++ resources/views/voitures/showForm.blade.php
@@ -1,3 +1,4 @@
+<?php use Carbon\Carbon; ?>
 @extends('layouts.app')
 
 @section('content')
@@ -22,17 +23,23 @@
                             </ul>
                         </div>
                     @endif
-
-                    <form method="POST" action="{{ route('store') }}" enctype="multipart/form-data">
+                    <form method="POST" action="<?= isset($voiture) && !empty($voiture) ? route('update',['id'=>$voiture->id]) : route('store') ?>" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group row">
                             <label class="col-sm-3 col-form-label">Marque</label>
                             <div class="col-sm-9">
                             <select class="form-control" id="status-select" name="marque" onchange="refreshList();">
-                                <option>--Choisissez une marque--</option>
-                                @foreach($marque as $uneMarque)
-                                    <option value="{{$uneMarque->id}}">{{$uneMarque->nom}}</option>
-                                @endforeach
+                                    @if(!empty($voiture))
+                                        <option value="{{$voiture->marques->id}}">{{$voiture->marques->nom}}</option>
+                                    @else
+                                        <option>--Choisissez une marque--</option>
+                                    @endif
+                                    @foreach($marque as $uneMarque)
+                                        @if(!empty($voiture) && isset($voiture) && $voiture->marques->id === $uneMarque->id)
+                                        @else
+                                            <option value="{{$uneMarque->id}}">{{$uneMarque->nom}}</option>
+                                        @endif
+                                    @endforeach
                             </select>
                             </div>
                         </div>
@@ -40,55 +47,59 @@
                             <label class="col-sm-3 col-form-label">Modèle</label>
                             <div class="col-sm-9">
                             <select class="form-control" id="status-select2" name="modele">
-                                <option>--Choisissez un modele--</option>
-                                @foreach($modele as $unModele)
-                                    <option value="{{$unModele->id}}">{{$unModele->nom}}</option>
-                                @endforeach
+                                @if(!empty($voiture))
+                                    <option value="{{$voiture->modeles->id}}">{{$voiture->modeles->nom}}</option>
+                                @else
+                                    <option>--Choisissez un modèle--</option>
+                                @endif
                             </select>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-3 col-form-label">Date d'immatriculation</label>
                             <div class="col-sm-9">
-                                <input type="date" class="form-control" name="date">
+                                <input type="text" class="form-control" placeholder="<?= isset($voiture) && isset($voiture->date_immat) ? Carbon::parse($voiture->date_immat)->format('d/m/Y') : Carbon::today()->format('d/m/Y') ?>" onfocus="(this.type='date')" id="date" name="date" value="<?= isset($voiture) && isset($voiture->date_immat) ? $voiture->date_immat : Carbon::today() ?>">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-3 col-form-label">Prix /jour</label>
                             <div class="col-sm-9">
-                                <input type="number" class="form-control" name="prix" min="10">
+                                <input type="number" class="form-control" name="prix" min="10" value="<?= isset($voiture) && isset($voiture->prix) ? $voiture->prix : '' ?>">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-3 col-form-label">Couleur</label>
                             <div class="col-sm-9">
-                                <input type="color" class="form-control" name="couleur">
+                                <input type="color" class="form-control" name="couleur" value="<?= isset($voiture) && isset($voiture->couleur) ? $voiture->couleur : '' ?>">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-3 col-form-label">Description</label>
                             <div class="col-sm-9">
-                                <textarea type="text" class="form-control" name="description"></textarea>
+                                <textarea type="text" class="form-control" name="description"><?= isset($voiture) && isset($voiture->description) ? $voiture->description : '' ?></textarea>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-3 col-form-label">Kilometrage</label>
                             <div class="col-sm-9">
-                                <input type="number" class="form-control" name="kilometrage" min="10">
+                                <input type="number" class="form-control" name="kilometrage" min="10" value="<?= isset($voiture) && isset($voiture->kilometrage) ? $voiture->kilometrage : '' ?>">
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Image</label>
-                            <div class="col-sm-9">
-                                <input type="file" class="form-control-file" accept="image/png, image/jpeg, image/jpg" name="image" min="10">
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Image</label>
+                                <div class="col-sm-9">
+                                    <input type="file" class="form-control-file" accept="image/png, image/jpeg, image/jpg" name="image" min="10" value="<?= isset($voiture) && isset($voiture->image) ? $voiture->image : '' ?>">
+                                </div>
                             </div>
-                        </div>
 
                         <div class="form-group">
                             <img id="preview" class="img-fluid" src="#" alt="">
                         </div>
-
-                        <button type="submit" class="btn btn-primary mb-2">Ajouter véhicule</button>
+                        @if(isset($voiture) && !empty($voiture))
+                            <button type="submit" class="btn btn-primary mb-2">Modifier véhicule</button>
+                        @else
+                            <button type="submit" class="btn btn-primary mb-2">Ajouter véhicule</button>
+                        @endif
                     </form>
                 </div>
             </div>
