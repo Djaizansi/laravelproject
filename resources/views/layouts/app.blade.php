f0fb593b78b156c6d12fa80f27d95ba5998531ad
Djaizansi <youcef.jallali@gmail.com>
2020-06-28 23:54:55 +0200

    Ajout de lien en fonction de l'utilisateur

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,21 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <!-- Style Datatable -->
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" rel="stylesheet"> -->
+    <!-- Datatable.JS -->
+    
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css" integrity="sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q" crossorigin="anonymous">
 </head>
 <body>
@@ -27,11 +35,11 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'CarLocation') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
@@ -58,6 +66,13 @@
                                     </a>
 
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        
+                                        <a class="dropdown-item" href="{{ route('showMarque') }}">{{ __('Afficher les marques') }}</a>
+                                        <a class="dropdown-item" href="{{ route('formMarque') }}">{{ __('Ajouter une marque') }}</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="{{ route('showModele') }}">{{ __('Afficher les modeles') }}</a>
+                                        <a class="dropdown-item" href="{{ route('formModele') }}">{{ __('Ajouter un modele') }}</a>
+                                        <div class="dropdown-divider"></div>
                                         <a class="dropdown-item" href="{{ route('boutique') }}">{{ __('Afficher tous les véhicules') }}</a>
                                         <a class="dropdown-item" href="{{ route('form') }}">{{ __('Créer un véhicule') }}</a>
                                         <!-- <div class="dropdown-divider"></div> -->
@@ -67,11 +82,20 @@
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('boutique') }}">{{ __('Boutique') }} <i class="fas fa-store"></i></a>
                                 </li>
-                                
-                                <!-- <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('panier') }}">{{ __('Panier') }} <i class="fas fa-shopping-cart"></i></a>
-                                </li> -->
                             @endif
+                            <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ __('Location') }} <i class="fab fa-leanpub"></i><span class="caret"></span>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        @if (Auth::user()->roles === 'client')
+                                            <a class="dropdown-item" href="{{ route('showLocation') }}">{{ __('Mes emprunts') }}</a>
+                                        @elseif (Auth::user()->roles === 'admin')
+                                            <a class="dropdown-item" href="{{ route('showLocation') }}">{{ __('Afficher les emprunts') }}</a>
+                                        @endif
+                                    </div>
+                                </li>
 
                             <li class="nav-item dropdown">
                                 
